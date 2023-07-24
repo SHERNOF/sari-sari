ff3928b7d66374bae5c777e2b7128d73b535d1fa
shernof <sherwin.nofuente@yahoo.com>
2023-07-24 22:44:17 +1000

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">sari-sari</a>
+      </header>
+      <main>Product List</main>
+    </div>
+  );
+}
+
+export default App;
